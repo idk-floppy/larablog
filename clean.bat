9f3909bd0a6bf772adffd71257260b77931d7311
ADHD is DEV <webtomekk@gmail.com>
2023-02-09 17:20:13 +0100

    Update clean.bat

--- clean.bat
+++ clean.bat
@@ -1,20 +1,18 @@
 @ECHO OFF
 CLS
-ECHO Running commands...
-START /B /WAIT CMD /c timeout 10
+ECHO Preparing
+START /B /WAIT CMD /c timeout 5
 START /B /WAIT CMD /c COPY .env.example .env
 echo Clean .env file created
 START /B /WAIT CMD /c composer install
 START /B /WAIT CMD /c npm install
 echo Composer and NPM dependencies installed
-START /B /WAIT CMD /c npm run build
-echo NPM build finished
-START /B /WAIT CMD /c php artisan migrate:fresh --seed
-echo Migration and seeding finished
 START /B /WAIT CMD /c php artisan cache:clear
 echo Cache deleted
 START /B /WAIT CMD /C php artisan key:generate
 echo New key generated
 START /B /WAIT CMD /C php artisan route:clear
 echo Route list cleared
+START /B /WAIT CMD /c php artisan migrate:fresh --seed
+echo Migration and seeding finished
 echo Now you can go ahead and run 'php artisan serve'
